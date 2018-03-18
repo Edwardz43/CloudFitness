9c6bac2890210db82fe13a2776aea99c50dd0f89
EdLo <b93303015@gmail.com>
2018-03-18 16:57:28 +0800

    2018/03/18 16:58 新增 : 歷史紀錄頁面 暫用expandable list view呈現 加入DEMO資料

--- app/src/main/java/tw/brad/apps/cloudfitness/HistoricalDataActivity.java
+++ app/src/main/java/tw/brad/apps/cloudfitness/HistoricalDataActivity.java
@@ -2,9 +2,28 @@ package tw.brad.apps.cloudfitness;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import tw.brad.apps.cloudfitness.java_class.MyExpandableListAdapter;
 
 public class HistoricalDataActivity extends AppCompatActivity {
+    private MyExpandableListAdapter listAdapter;
+    private ExpandableListView expListView;
+    private List<String> listDataHeader;
+    private HashMap<String, List<String[]>> listDataChild;
+    private final String[] mMonths =
+            new String[] {"November, 2017", "December, 2017", "January, 2018"};
+    //, "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Okt"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +35,80 @@ public class HistoricalDataActivity extends AppCompatActivity {
 
     private void init() {
         //TODO
+        // 取得list view
+        expListView = (ExpandableListView) findViewById(R.id.expand_list);
+        // 準備list資料
+        prepareListData();
+
+        // 設置調變器
+        listAdapter = new MyExpandableListAdapter(this, listDataHeader, listDataChild);
+        expListView.setAdapter(listAdapter);
+    }
+
+    // 配置資料
+    private void prepareListData() {
+        listDataHeader = new ArrayList<>();
+        listDataChild = new HashMap<>();
+
+        // 設置子項目header : 月份
+        for (String mMonth : mMonths) {
+            listDataHeader.add(mMonth);
+            Log.d("ed43", mMonth);
+        }
+
+        // 測試資料 : 11月
+        List<String[]> nov = new ArrayList<>();
+
+        String[] nov1 = new String[]{
+                "11/11/2017", "Weight 200 lb, BMI 21, Body Fat 36.4%",
+                "Muscle Mass 100 lb, Body Water 57.5%"
+        };
+        String[] nov2 = new String[]{
+                "11/14/2017", "Weight 203 lb, BMI 22, Body Fat 38.4%",
+                "Muscle Mass 101 lb, Body Water 57.9%"
+        };
+        String[] nov3 = new String[]{
+                "11/23/2017", "Weight 208 lb, BMI 23, Body Fat 39.0%",
+                "Muscle Mass 103 lb, Body Water 58.5%"
+        };
+        nov.add(nov1);nov.add(nov2);nov.add(nov3);
+
+        // 測試資料 : 12月
+        List<String[]> dec = new ArrayList<>();
+        String[] dec1 = new String[]{
+                "12/12/2017", "Weight 203 lb, BMI 21, Body Fat 36.4%",
+                "Muscle Mass 100 lb, Body Water 57.5%"
+        };
+        String[] dec2 = new String[]{
+                "12/15/2017", "Weight 206 lb, BMI 22, Body Fat 38.4%",
+                "Muscle Mass 101 lb, Body Water 57.9%"
+        };
+        String[] dec3 = new String[]{
+                "12/30/2017", "Weight 208 lb, BMI 23, Body Fat 39.0%",
+                "Muscle Mass 103 lb, Body Water 58.5%"
+        };
+        String[] dec4 = new String[]{
+                "12/31/2017", "Weight 205 lb, BMI 23, Body Fat 39.0%",
+                "Muscle Mass 103 lb, Body Water 58.5%"
+        };
+        dec.add(dec1);dec.add(dec2);dec.add(dec3);dec.add(dec4);
+
+        // 測試資料 : 1月
+        List<String[]> jan = new ArrayList<>();
+        String[] jan1 = new String[]{
+                "1/1/2018", "Weight 203 lb, BMI 21, Body Fat 36.4%",
+                "Muscle Mass 100 lb, Body Water 57.5%"
+        };
+        String[] jan2 = new String[]{
+                "1/6/2018", "Weight 206 lb, BMI 22, Body Fat 38.4%",
+                "Muscle Mass 100 lb, Body Water 57.5%"
+        };
+        jan.add(jan1);jan.add(jan2);
+
+        listDataChild.put(listDataHeader.get(0), nov);
+        listDataChild.put(listDataHeader.get(1), dec);
+        listDataChild.put(listDataHeader.get(2), jan);
+        Log.d("ed43", new Gson().toJson(listDataChild));
     }
 
     //按鈕 : 返回鍵
---
+++ app/src/main/java/tw/brad/apps/cloudfitness/java_class/MyExpandableListAdapter.java
@@ -0,0 +1,119 @@
+package tw.brad.apps.cloudfitness.java_class;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import tw.brad.apps.cloudfitness.R;
+
+/**
+ * Created by EdLo on 2018/3/18.
+ */
+
+public class MyExpandableListAdapter extends BaseExpandableListAdapter {
+
+    private Context _context;
+    private List<String> _listDataHeader; // header titles
+    // child data in format of header title, child title
+    private HashMap<String, List<String[]>> _listDataChild;
+
+    public MyExpandableListAdapter(Context context, List<String> listDataHeader,
+                                 HashMap<String, List<String[]>> listChildData) {
+        this._context = context;
+        this._listDataHeader = listDataHeader;
+        this._listDataChild = listChildData;
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosititon) {
+        return this._listDataChild.get(this._listDataHeader.get(groupPosition))
+                .get(childPosititon);
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, final int childPosition,
+                             boolean isLastChild, View convertView, ViewGroup parent) {
+
+        final String[] childText = (String[]) getChild(groupPosition, childPosition);
+
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.list_item, null);
+        }
+
+        TextView txtListChild = (TextView) convertView
+                .findViewById(R.id.lblListDate);
+        TextView txtListChild2 = (TextView) convertView
+                .findViewById(R.id.lblListData1st);
+        TextView txtListChild3 = (TextView) convertView
+                .findViewById(R.id.lblListData2nd);
+        txtListChild.setText(childText[0]);
+        txtListChild2.setText(childText[1]);
+        txtListChild3.setText(childText[2]);
+        return convertView;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return this._listDataChild.get(this._listDataHeader.get(groupPosition))
+                .size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return this._listDataHeader.get(groupPosition);
+    }
+
+    @Override
+    public int getGroupCount() {
+        return this._listDataHeader.size();
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded,
+                             View convertView, ViewGroup parent) {
+        String headerTitle = (String) getGroup(groupPosition);
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.list_group, null);
+        }
+
+        TextView lblListHeader = (TextView) convertView
+                .findViewById(R.id.list_header);
+        lblListHeader.setTypeface(null, Typeface.BOLD);
+        lblListHeader.setText(headerTitle);
+
+        return convertView;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
